9bec62d78bf77a9bf7525054021dbad5fb247c84
Denis Leme <denis_leme@icloud.com>
2023-08-21 17:45:14 -0300

    validações de usuario

--- Class/Account.ts
+++ Class/Account.ts
@@ -1,7 +1,8 @@
 export abstract class Account {
-    name: string
+    private name: string
     accountNumber: number
     balance: number
+    status: boolean = true
 
     constructor(name:string, accountNumber: number){
         this.name = name
@@ -9,13 +10,32 @@ export abstract class Account {
         this.balance = 0
     }
 
-    deposit =():void =>{
-        console.log('Você Depositou')
+    setName = (name: string):void =>{
+        this.name = name
+        console.log('Nome alterado com sucesso!!')
+
+    }
+    getName = ():string =>{
+        return this.name
+    }
+
+    deposit = ():void =>{
+        if(this.validateStatus()){
+            console.log('Você Depositou')
+        }
     }
     withdraw = ():void  => {
         console.log('Você Sacou')
     }
     getBalance = ():void  =>{
         console.log(this.balance)
+    }
+
+    private validateStatus = ():boolean =>{
+        if(this.status){
+            return this.status
+        }
+       throw new Error('Conta Inválida')
+
     }
 }
--- app.ts
+++ app.ts
@@ -7,9 +7,14 @@ import { PessoaAccount } from './Class/PessoaAccount'
 
 
 const pessoaAccount: PessoaAccount = new PessoaAccount(1, 'Denis', 1)
+console.log(pessoaAccount)
 pessoaAccount.deposit()
 
+// pessoaAccount.setName('Denis Leme')
+// console.log(pessoaAccount)
+// console.log(pessoaAccount.getName())
 
-const companyAccount: CompanyAccount = new CompanyAccount('Leme', 3)
-console.log(companyAccount.deposit())
+
+//const companyAccount: CompanyAccount = new CompanyAccount('Leme', 3)
+//console.log(companyAccount.deposit())
 
